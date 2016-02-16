daa949deedb00ad7b3f1b7bf378d59a650b6abcc
harleymckee <h@harley.website>
2016-02-15 18:35:20 -0600

    fixed front end loop with busy boolean

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,22 +1,24 @@
 defmodule Chat.RoomChannel do
   use Phoenix.Channel
   require Logger
-  alias Editor.Repo
+  alias Editor.State
 
   def join("topic:" <> _topic_name, _auth_msg, socket) do
+    # 
     send(self, :after_join)
     {:ok, socket}
   end
 
   def handle_info(:after_join, socket) do
-    result = Repo._get_agent
+    result = State._get_agent
     broadcast! socket, "message", %{body: result}
     {:noreply, socket}
   end
 
   def handle_in("message", %{"body" => body}, socket) do
+    IO.puts body
     broadcast! socket, "message", %{body: body}
-    Repo.run(body)
+    State.run(body)
     {:noreply, socket}
   end
 end
