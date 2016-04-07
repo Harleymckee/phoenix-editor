6e321be904c6d74b1bda9550113577696594d764
harleymckee <h@harley.website>
2016-04-06 23:43:15 -0500

    test

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,7 +2,6 @@ defmodule Chat.RoomChannel do
   use Phoenix.Channel
   require Logger
   alias Editor.State
-
   def join("topic:" <> _topic_name, _auth_msg, socket) do
     send(self, :after_join)
     {:ok, socket}
@@ -10,18 +9,12 @@ defmodule Chat.RoomChannel do
 
   def handle_info(:after_join, socket) do
     result = State._get_agent
-    broadcast! socket, "initial state", %{body: result}
+    push socket, "initial state", %{body: result}
     {:noreply, socket}
   end
 
   def handle_in("message", %{"body" => body}, socket) do
     broadcast! socket, "message", %{body: body}
-    State.run(body)
-    {:noreply, socket}
-  end
-
-  def handle_out("message", %{"body" => body}, socket) do
-    push socket, "message", body
     State.run(body)
     {:noreply, socket}
   end
